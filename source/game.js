62ab5cc58596a355f30f18a0e9660836bb857752
Marcin Skrzyński <marcin.skrzynski@likims.com>
2018-11-08 14:37:00 +0100

    Add chunks for separate html files

---
+++ source/game.js
@@ -0,0 +1,12 @@
+import 'normalize.css';
+import './style/style.scss';
+import './style/style.css';
+
+import { Engine } from './game/engine.js';
+
+new Engine({
+  background: 0x1099bb,
+  container: 'js-game',
+  height: 630,
+  width: 1120
+});
--- source/index.js
+++ source/index.js
@@ -1,16 +1,7 @@
-import "normalize.css";
-import "./style/style.sass";
-import "./style/style.css";
+import 'normalize.css';
+import './style/style.scss';
+import './style/style.css';
 
-// import { Engine } from "./game/engine.js";
-//
-// new Engine({
-//   background: 0x1099bb,
-//   container: "js-game",
-//   height: 630,
-//   width: 1120
-// });
-
-import Popup from "./game/popup.js";
-const popupHandlers = document.querySelectorAll("[data-open-popup]");
+import Popup from './ui-components/popup.js';
+const popupHandlers = document.querySelectorAll('[data-open-popup]');
 [...popupHandlers].map(item => Popup.attach(item));
